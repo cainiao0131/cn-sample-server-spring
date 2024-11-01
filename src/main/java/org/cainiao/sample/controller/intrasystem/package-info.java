a4e7bbbbe9745f0858e06bfd6b90410f2f497726
cainiao <wdhlzd@163.com>
2024-11-01 10:55:21 +0800

    controller 包名调整
    Signed-off-by: cainiao <wdhlzd@163.com>

--- src/main/java/org/cainiao/sample/controller/intrasystem/package-info.java
+++ src/main/java/org/cainiao/sample/controller/intrasystem/package-info.java
@@ -1,9 +1,9 @@
 /**
- * 由同一【系统】中的其它【服务端应用】调用的接口<br />
+ * 系统内调用接口，由同一【系统】中的其它【服务端应用】调用<br />
  * 将当前应用配置为 Client Credential 类型的资源服务器，Filter Chain 的工作：<br />
  * <ol>
  *     <li>校验请求头中的 ClientId 与 ClientSecret</li>
  *     <li>对比请求头中的 ClientId 与当前【系统】的 ClientId，只允许来自当前【系统】的请求</li>
  * </ol>
  */
-package org.cainiao.sample.controller.system;
+package org.cainiao.sample.controller.intrasystem;
